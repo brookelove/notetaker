7d457c3f8389c1932a2bce9ef80a57ce72a1b61d
Brooke Love <savienloveb@gmail.com>
2022-04-29 12:49:02 -0700

    starting heroku

--- routes/HTMLroutes.js
+++ routes/HTMLroutes.js
@@ -4,11 +4,11 @@ const router = require('express').Router();
 // const controller = require('../db.controller.js');
 
 router.get ('/notes', (req,res) =>{
-    res.sendFile(path.join(__dirname), '../public/notes.html');
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
 })
 
 router.get ('*', (req,res) =>{
-    res.sendFile(path.join(__dirname), '../public/notes.html');
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const allRoutes = require("./routes/")
+const allRoutes = require("./routes")
+const path = require('path');
 
 //parsing and routing middleware 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+// app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(allRoutes);
+
 
 //starts the server
 app.listen(PORT,() =>{
